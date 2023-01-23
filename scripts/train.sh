b2e83a12b31922aee580235e99d22fba47cfd673
Hariz Izzat <harizizzat09@gmail.com>
2023-01-23 18:55:52 +0800

    update train.sh file

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # run this script in the root path of TransMVSNet
-MVS_TRAINING="/data/DTU/mvs_training/dtu/" # path to dataset mvs_training
+MVS_TRAINING="./data/data/mvs_training/dtu/" # path to dataset mvs_training
 LOG_DIR="./outputs/dtu_training" # path to checkpoints
 if [ ! -d $LOG_DIR ]; then
 	mkdir -p $LOG_DIR
 fi
 
-NGPUS=8
+NGPUS=1
 BATCH_SIZE=1
-python -m torch.distributed.launch --nproc_per_node=$NGPUS train.py \
+./env/Scripts/python -m torch.distributed.launch --nproc_per_node=$NGPUS train.py \
 	--logdir=$LOG_DIR \
 	--dataset=dtu_yao \
 	--batch_size=$BATCH_SIZE \
@@ -23,3 +23,4 @@ python -m torch.distributed.launch --nproc_per_node=$NGPUS train.py \
 	--depth_inter_r="4.0,1.0,0.5" \
 	--lrepochs="6,8,12:2" \
 	--dlossw="1.0,1.0,1.0" | tee -a $LOG_DIR/log.txt
+	
